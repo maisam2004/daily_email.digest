03a000c1bad4961aeb9e7da5fa8ded62f845841d
maisam2004 <maisam_2004@hotmail.com>
2023-06-14 16:04:13 +0100

    Add qui class and config

---
+++ o_gui.py
@@ -0,0 +1,18 @@
+class Daily_digest_gui:
+
+    def __init__(self,root):
+        pass 
+
+    """
+        enable admin to :
+        configure which content source to include in email
+        -add recipients
+        -remove re
+        -schedule daily time 
+        -configuire sender
+
+    """
+    if __name__== '__main___':
+        root = TK()
+        app=Daily_digest_gui(root)
+        root.mainloop()
